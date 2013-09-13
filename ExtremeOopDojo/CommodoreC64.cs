a320eb6e99452fd5d45959c388afe45973854b0d
Martin Wåger <martinw@gmail.com>
2013-09-13 10:42:48 +0200

    First test

---
+++ ExtremeOopDojo/CommodoreC64.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExtremeOopDojo
+{
+    public class CommodoreC64
+    {
+        public string Interpret(string empty)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
