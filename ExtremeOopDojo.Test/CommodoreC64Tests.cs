2b0673b08091555e97f79190ed6d09259ddc6bc7
Martin Wåger <martin.wager@dynabyte.se>
2013-11-25 13:32:25 +0100

    Added all the tests

--- ExtremeOopDojo.Test/CommodoreC64Tests.cs
+++ ExtremeOopDojo.Test/CommodoreC64Tests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ExtremeOopDojo.Test
 {
     [TestClass]
-    public class CommodoreC64Test
+    public class CommodoreC64Tests
     {
         [TestMethod]
         public void InterpretShouldHandleParseErrors()
@@ -20,7 +20,7 @@ namespace ExtremeOopDojo.Test
         }
 
         [TestMethod]
-        public void InterpretShouldReturnEmptyWhenGivenEmpty()
+        public void InterpretShouldReturnEmptyWhenGivenEmpty() // An empty program produces no output
         {
             //Arrange
             var classUnderTest = new CommodoreC64();
@@ -33,7 +33,7 @@ namespace ExtremeOopDojo.Test
         }
 
         [TestMethod]
-        public void InterpretShouldReturnNewLineWhenGivenPrint()
+        public void InterpretShouldReturnNewLineWhenGivenPrint() // A bare “print” statement produces a single newline
         {
             //Arrange
             var classUnderTest = new CommodoreC64();
@@ -46,7 +46,7 @@ namespace ExtremeOopDojo.Test
         }
 
         [TestMethod]
-        public void InterpretShouldReturnStringWhenGivenString()
+        public void InterpretShouldReturnStringWhenGivenString() // A “print” statement can have a constant string as an argument. The output is the constant string
         {
             //Arrange
             var classUnderTest = new CommodoreC64();
@@ -59,7 +59,7 @@ namespace ExtremeOopDojo.Test
         }
 
         [TestMethod]
-        public void InterpretShouldHandleMultipleStatements()
+        public void InterpretShouldHandleMultipleStatements() // Two or more statements in a sequence are executed one after the other
         {
             //Arrange
             var classUnderTest = new CommodoreC64();
@@ -68,14 +68,14 @@ namespace ExtremeOopDojo.Test
             var result = classUnderTest.Interpret(input);
 
             //Assert
-            Assert.AreEqual( Environment.NewLine+"Hi" + Environment.NewLine +
+            Assert.AreEqual(Environment.NewLine + "Hi" + Environment.NewLine +
                             "There" + Environment.NewLine +
                             "!" + Environment.NewLine,
                             result);
         }
 
         [TestMethod]
-        public void InterpretShouldHandlePrintWithIntegerOperand()
+        public void InterpretShouldHandlePrintWithIntegerOperand() // : The “print” statement can output number constants.
         {
             //Arrange
             var classUnderTest = new CommodoreC64();
@@ -84,7 +84,7 @@ namespace ExtremeOopDojo.Test
             var result = classUnderTest.Interpret(input);
 
             //Assert
-            Assert.AreEqual("123" + Environment.NewLine,result);
+            Assert.AreEqual("123" + Environment.NewLine, result);
         }
 
         [TestMethod]
@@ -101,7 +101,7 @@ namespace ExtremeOopDojo.Test
         }
 
         [TestMethod]
-        public void InterpretShouldHandlePrintOfVariables()
+        public void InterpretShouldHandlePrintOfVariables() // A single letter is a variable. The print statement can print its value. The default value for a variable 0
         {
             //Arrange
             var classUnderTest = new CommodoreC64();
@@ -114,7 +114,7 @@ namespace ExtremeOopDojo.Test
         }
 
         [TestMethod]
-        public void InterpretShouldHandleVariableAssignment()
+        public void InterpretShouldHandleVariableAssignment() // An assignment statement binds a value to a variable.
         {
             //Arrange
             var classUnderTest = new CommodoreC64();
@@ -127,7 +127,7 @@ namespace ExtremeOopDojo.Test
         }
 
         [TestMethod]
-        public void InterpretShouldHandleAddition()
+        public void InterpretShouldHandleAddition() // Two numeric constants can be added together.
         {
             //Arrange
             var classUnderTest = new CommodoreC64();
@@ -137,6 +137,58 @@ namespace ExtremeOopDojo.Test
 
             //Assert
             Assert.AreEqual("10" + Environment.NewLine, result);
+        }
+
+        [TestMethod]
+        public void InterpretShouldHandleMultipleAddition() // A numeric expression can have more than two terms.
+        {
+            //Arrange
+            var classUnderTest = new CommodoreC64();
+            var input = new UserInput(@"PRINT 4 + 4 + 12");
+            //Act
+            var result = classUnderTest.Interpret(input);
+
+            //Assert
+            Assert.AreEqual("20" + Environment.NewLine, result);
+        }
+
+        [TestMethod]
+        public void InterpretShouldHandleVariablesAndConstants() // A numeric expression can be built with variables and/or constants
+        {
+            //Arrange
+            var classUnderTest = new CommodoreC64();
+            var input = new UserInput(@"A=2;B=7;PRINT A + 1;PRINT A + B;PRINT 99 + B");
+            //Act
+            var result = classUnderTest.Interpret(input);
+
+            //Assert
+            Assert.AreEqual("3" + Environment.NewLine + "9" + Environment.NewLine + "106" + Environment.NewLine, result);
+        }
+
+        [TestMethod]
+        public void InterpretShouldHandleSubtraction() // Two numeric expressions can be subtracted
+        {
+            //Arrange
+            var classUnderTest = new CommodoreC64();
+            var input = new UserInput(@"PRINT 1 - 2");
+            //Act
+            var result = classUnderTest.Interpret(input);
+
+            //Assert
+            Assert.AreEqual("-1" + Environment.NewLine, result);
+        }
+
+        [TestMethod]
+        public void InterpretShouldHandleParenthesis() // Expressions can be parenthesized
+        {
+            //Arrange
+            var classUnderTest = new CommodoreC64();
+            var input = new UserInput(@"PRINT 1 - (2 + 3)");
+            //Act
+            var result = classUnderTest.Interpret(input);
+
+            //Assert
+            Assert.AreEqual("-1" + Environment.NewLine, result);
         }
     }
 }
