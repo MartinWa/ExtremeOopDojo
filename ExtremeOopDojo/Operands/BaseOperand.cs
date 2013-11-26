22494c8f28e3f1bcc7ca09fa3adc1212e8bfc07d
Martin Wåger <martinw@gmail.com>
2013-11-26 10:15:54 +0100

    Refactored to remove imperative functions, now they are at the correct place as factory methods

--- ExtremeOopDojo/Operands/BaseOperand.cs
+++ ExtremeOopDojo/Operands/BaseOperand.cs
@@ -1,7 +1,37 @@
-﻿namespace ExtremeOopDojo.Operands
+﻿using System;
+using System.Data;
+using System.Text.RegularExpressions;
+
+namespace ExtremeOopDojo.Operands
 {
     public abstract class BaseOperand
     {
+        public static BaseOperand FromOperand(string operand)
+        {
+            var stringOperand = Regex.Match(operand, @"""(?<string>.*)""");
+            var integerOperand = Regex.Match(operand, @"\s*(?<integer>-*\d+)\s*");
+            var variableOperand = Regex.Match(operand, @"\s*(?<variable>[a-zA-Z]+)\s*");
+            if (string.IsNullOrEmpty(operand))
+            {
+                return new EmptyOperand();
+            }
+            if (stringOperand.Success)
+            {
+                var str = stringOperand.Groups["string"];
+                return new StringOperand(str.Value);
+            }
+            if (integerOperand.Success)
+            {
+                var integer = integerOperand.Groups["integer"];
+                return IntegerOperand.FromString(integer.Value);
+            }
+            if (variableOperand.Success)
+            {
+                var variable = variableOperand.Groups["variable"];
+                return new VariableOperand(variable.Value);
+            }
+            throw new InvalidExpressionException(String.Format("{0} is not a valid operand", operand));
+        }
         public abstract override string ToString(); 
     }
 }
--- ExtremeOopDojo/UserInput.cs
+++ ExtremeOopDojo/UserInput.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
-using System.Text.RegularExpressions;
 using ExtremeOopDojo.Command;
-using ExtremeOopDojo.Operands;
 
 namespace ExtremeOopDojo
 {
@@ -20,56 +17,7 @@ namespace ExtremeOopDojo
         public IEnumerable<BaseCommand> Parse()
         {
             var expressions = _input.Split(new[] { ';' }, StringSplitOptions.None);
-            return expressions.Select(ParseExpression).ToList();
-        }
-
-        private BaseCommand ParseExpression(string expression)
-        {
-            var printOperator = Regex.Match(expression, @"PRINT(?<operand>.*)");
-            var variableOperator = Regex.Match(expression, @"(?<variable>[a-zA-Z]+)\s*=\s*(?<value>.+)");
-            if (string.IsNullOrEmpty(expression))
-            {
-                return new EmptyCommand();
-            }
-            if (printOperator.Success)
-            {
-                var operand = printOperator.Groups["operand"];
-                return new PrintCommand(ParsePrintOperand(operand.Value));
-            }
-            if (variableOperator.Success)
-            {
-                var variableName = variableOperator.Groups["variable"];
-                var variableValue = variableOperator.Groups["value"];
-                return VariableCommand.FromString(variableName.Value, variableValue.Value);
-            }
-            throw new InvalidExpressionException(String.Format("{0} is not a valid expression", expression));
-        }
-
-        private BaseOperand ParsePrintOperand(string operand)
-        {
-            var stringOperand = Regex.Match(operand, @"""(?<string>.*)""");
-            var integerOperand = Regex.Match(operand, @"\s*(?<integer>-*\d+)\s*");
-            var variableOperand = Regex.Match(operand, @"\s*(?<variable>[a-zA-Z]+)\s*");
-            if (string.IsNullOrEmpty(operand))
-            {
-                return new EmptyOperand();
-            }
-            if (stringOperand.Success)
-            {
-                var str = stringOperand.Groups["string"];
-                return new StringOperand(str.Value);
-            }
-            if (integerOperand.Success)
-            {
-                var integer = integerOperand.Groups["integer"];
-                return IntegerOperand.FromString(integer.Value);
-            }
-            if (variableOperand.Success)
-            {
-                var variable = variableOperand.Groups["variable"];
-                return new VariableOperand(variable.Value);
-            }
-            throw new InvalidExpressionException(String.Format("{0} is not a valid operand", operand));
+            return expressions.Select(BaseCommand.FromExpression).ToList();   // Using eager ToList() to avoid lazy evaluation giving parsing exceptions later
         }
     }
 }
